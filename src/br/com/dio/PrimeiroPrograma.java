42aaebc48c76dadd7cdbeb91ed05a7198a73d06a
CheeseOmelette <poketvgameplays@gmail.com>
2023-03-12 17:11:27 -0300

    com

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato("JACK", "Branca", 5);
+
+        System.out.println(gato);
+
+    }
+}
